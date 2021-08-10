1ab690a8991d9588433147d436fdebb119a5c70d
C4ArtZ <dev@c4artz.de>
2021-08-10 10:44:18 +0200

    sexy pixels

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> -->
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     <title>c4artz.de</title>
 </head>
-<body>
-    <div class="container">
 
-        <h1>C4ARTZ</h1>
+<body class="noselect">
+    <div class="hero">
+
+        <div class="container">
+            <h1>C4ARTZ</h1>
+            <p>Gamer, Developer, Self hoster</p>
+        </div>
+
+        <div class="stuff">
+            <p>What do I do?</p>
+            <img src="{{ asset('assets/arrow-hover.svg') }}" alt="">
+        </div>
+    </div>
+    <div class="projects">
+        <div class="project">
+            I bims 1 Projekt!
+        </div>
     </div>
 </body>
+
 </html>
