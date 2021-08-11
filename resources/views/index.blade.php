4b246e1e86ecdf596b87ef6440c19b140321c856
C4ArtZ <dev@c4artz.de>
2021-08-11 15:24:42 +0200

    several accessability and responsiveness fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#111827">
-    <meta name="description" content="C4ArtZ is a gamer, self taught C#, JS and PHP developer and avid self hoster.">
+    <meta name="description" content="C4ArtZ ist ein Gamer, selbst gelernter C#, JS und PHP Entwickler und eifriger Self hoster.">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
@@ -17,12 +17,8 @@
 
 <body class="noselect">
     <div class="hero">
-
         <div class="container">
-            <div class="title">
-
-                <h1>C4ARTZ</h1>
-            </div>
+            <h1>C4ARTZ</h1>
             <p>Gamer, Developer, Self hoster</p>
         </div>
     </div>
